f009b4cb2f5e37f6c12f561379ed7d7f45ef99f0
Oliwia <oliwia.m.96@wp.pl>
2018-03-01 20:11:57 +0100

    Start of journey

--- today.py
+++ today.py
@@ -30,11 +30,70 @@ def main():
         else:
             line = line.strip('\n')
             row = line.split(' ')
-            rides.append({'a': row[0], 'b': row[1], 'x': row[2], 'y': row[3],'s': row[4], 'f': row[5]})
+            rides.append({'a': int(row[0]), 'b': int(row[1]), 'x': int(row[2]), 'y': int(row[3]),'s': int(row[4]), 'f': int(row[5]), 'status': 0})
+            # 0 not done
+            # 1 in progress
+            # 2 done
         i += 1
     print(str(R))
     print(rides)
     readfile.close()
+
+    miasto = []
+    for i in range(R):
+        row = []
+        for j in range(C):
+            row.append(1)
+        miasto.append(row)
+
+    print(miasto)
+
+    vehicles = []
+    for i in range(F):
+        vehicles.append({'x1': 0, 'y1': 0, 'x2': 0, 'y2': 0, 'status': 0, 'podroze': []})
+            # 0 zupelnie wolny
+            # 1 jedzie do klienta
+            # 2 jedzie z klientem
+
+
+    for t in range(T):
+        for i in range(len(rides)):
+            podroz = rides[i]
+            #najpierw przydziel
+            for j in range(len(vehicles)):
+                pojazd = vehicles[j]
+                if(pojazd['status'] == 0):
+                    pojazd['status'] = 1
+                    pojazd['x2'] = podroz['a']
+                    pojazd['y2'] = podroz['b']
+                    podroz['status'] = 1
+                    pojazd['podroze'].append(i)
+                    break
+        #potem przesun pojazdy
+        for pojazd in vehicles:
+            if pojazd['x1'] < pojazd['x2']:
+                pojazd['x1'] += 1
+            elif pojazd['x1'] > pojazd['x2']:
+                pojazd['x1'] -= 1
+            else:
+                if pojazd['y1'] < pojazd['y2']:
+                    pojazd['y1'] += 1
+                elif pojazd['y1'] > pojazd['y2']:
+                    pojazd['y1'] -= 1
+                else:
+                    #dojechal do celu
+                    if pojazd['status'] == 1:
+                        #dojechal do klienta
+                        indexPodrozy = pojazd['podroze'][-1]
+                        podroz = rides[indexPodrozy]
+                        pojazd['x2'] = podroz['x']
+                        pojazd['y2'] = podroz['y']
+                    elif pojazd['status'] == 2:
+                        print("siema")
+                        # sukces!!!
+
+
+
 
 
 
