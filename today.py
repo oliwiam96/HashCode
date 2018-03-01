21c3df8c83b028cf3cbbdfc6dc716d32fb0a5f2e
Matek1731 <matek.sl@wp.pl>
2018-03-01 19:00:05 +0100

    Dataset

--- today.py
+++ today.py
@@ -2,4 +2,28 @@ import matplotlib
 import scipy
 import numpy
 
-print("Hello world")
+
+def openReadableFile(nazwa):
+    f = open(nazwa, "r")
+    return f
+
+def main():
+    i = 0
+    readfile = open("a_example.in", "r")
+    metadane = []
+    for line in readfile:
+        print(line)
+
+        if i == 0:
+            metadane = line
+        i += 1
+    print(metadane)
+
+    print("Hello world")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
