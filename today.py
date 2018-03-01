66d38d1d23377b1244975f1df4223b9529c56c35
Oliwia <oliwia.m.96@wp.pl>
2018-03-01 19:05:32 +0100

    Read first numbers

--- today.py
+++ today.py
@@ -10,12 +10,31 @@ def openReadableFile(nazwa):
 def main():
     i = 0
     readfile = open("a_example.in", "r")
+
     metadane = []
+    R=0
+    C=0
+    F=0
+    N=0
+    B=0
+    T=0
     for line in readfile:
         print(line)
 
         if i == 0:
             metadane = line
+            metadane = metadane.split(' ')
+            R = int(metadane[0])
+            C = int(metadane[1])
+            F = int(metadane[2])
+            N = int(metadane[3])
+            B = int(metadane[4])
+            T = int(metadane[5])
+
+
+
+        else:
+            print("hel")
         i += 1
     print(metadane)
 
