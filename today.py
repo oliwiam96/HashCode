5f4aaa0a86298e7ee1e1df834065a1f7639a98f1
Matek1731 <matek.sl@wp.pl>
2018-03-01 19:07:52 +0100

    merge z Oli

--- today.py
+++ today.py
@@ -11,13 +11,22 @@ def main():
     i = 0
     readfile = open("a_example.in", "r")
     metadane = []
+    miasto = []
     for line in readfile:
         print(line)
-
         if i == 0:
             metadane = line
+
+        else:
+            line = line.strip('\n')
+            row = line.split(' ')
+            print(row)
+
+
         i += 1
+
     print(metadane)
+    #print(miasto)
 
     print("Hello world")
 
